9f884bc086078eb197cb7b1d71cb728164e548d3
Andreea488 <andreea.iurean@gmail.com>
2025-08-23 23:47:00 +0300

    New commit backend

---
+++ src/test/java/pages/BasePage.java
@@ -0,0 +1,17 @@
+package pages;
+
+import helpers.ElementHelper;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class BasePage {
+    public WebDriver driver;
+    public ElementHelper elementHelper;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        this.elementHelper = new ElementHelper(driver);
+        PageFactory.initElements(driver, this);
+    }
+}
+
--- src/test/java/tests/GetAllBooksTest.java
+++ src/test/java/tests/GetAllBooksTest.java
@@ -3,18 +3,24 @@ package tests;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class GetAllBooksTest {
 
     @Test
-
     public void testMethod(){
-        RequestSpecification request = RestAssured.given();
-        Response response = request.get("https://demoqa.com/BookStore/v1/Books");
+
+        RequestSpecification requestSpecification = RestAssured.given();
+        requestSpecification.baseUri("https://demoqa.com");
+
+        Response response = requestSpecification.get("/BookStore/v1/Books");
         System.out.println(response.getStatusLine());
-        System.out.println(response.getBody());
-        System.out.println(response.getBody().asString());
+        response.getBody().prettyPrint();
+
+        Assert.assertEquals(response.getStatusCode(),200);
+        Assert.assertTrue(response.getStatusLine().contains("OK"));
+
 
     }
 }
